cf6334cda47543c6f99828001e4c5d9b2a1c8618
Renata Ribeiro <90521505+rbcribeiro@users.noreply.github.com>
2023-09-12 20:57:48 -0300

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 
 exports.port = process.argv[2] || process.env.PORT || 8080;
 exports.dbUrl = process.env.DATABASE_URL;
-exports.secret = process.env.JWT_SECRET;
-exports.adminEmail = process.env.ADMIN_EMAIL;
-exports.adminPassword = process.env.ADMIN_PASSWORD;
+exports.secret = process.env.JWT_SECRET || 'adminsecret';
+exports.adminEmail = process.env.ADMIN_EMAIL || 'admin@admin.com';
+exports.adminPassword = process.env.ADMIN_PASSWORD || '123456';
 
